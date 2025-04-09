bb827ade2fedc20225926ec916148ba5ad54febf
steffano908 <steffanogiacomocardenasjunes@gmail.com>
2025-04-09 10:04:53 -0500

    primer commit

---
+++ Aplica01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplica01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bienvenidos al curso de c#");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
